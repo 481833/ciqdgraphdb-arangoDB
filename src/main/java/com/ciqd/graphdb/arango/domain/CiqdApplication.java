cc5af563c5480a28dfd184fa9420f973064e5f85
Sudhakar Narasimhan <sudhakar.narasimhan@cognizant.com>
2019-09-09 16:08:52 +0530

    Removed EdgeRepository

--- src/main/java/com/ciqd/graphdb/arango/domain/CiqdApplication.java
+++ src/main/java/com/ciqd/graphdb/arango/domain/CiqdApplication.java
@@ -2,8 +2,6 @@ package com.ciqd.graphdb.arango.domain;
 
 import com.arangodb.springframework.annotation.Document;
 
-import javax.persistence.GeneratedValue;
-
 @Document
 public class CiqdApplication {
 
